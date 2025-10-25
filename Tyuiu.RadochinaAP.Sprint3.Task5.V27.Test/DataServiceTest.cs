9f4effaabf8170efb2ae8201747038567bc0eb1b
Tyuiu.RadochinaAP@outlook.coom <arinaradochinatyuiu@mail.ru>
2025-10-26 01:01:44 +0300

    .

---
+++ Tyuiu.RadochinaAP.Sprint3.Task5.V27.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.RadochinaAP.Sprint3.Task5.V27.Lib;
+namespace Tyuiu.RadochinaAP.Sprint3.Task5.V27.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumSumSeries()
+        {
+            DataService ds = new DataService();
+            int x = 5;
+            int startValue1 = 1;
+            int stopValue1 = 5;
+            int startValue2 = 1;
+            int stopValue2 = 10;
+
+            double res = ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2);
+            double wait = -1848380.055;
+
+            Assert.AreEqual(wait, res, 0.001);
+        }
+    }
+}
