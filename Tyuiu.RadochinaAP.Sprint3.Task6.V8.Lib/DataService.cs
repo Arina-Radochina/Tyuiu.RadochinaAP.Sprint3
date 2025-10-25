f410befa8e04799f9f59a423883cf5f6c1c36f50
Tyuiu.RadochinaAP@outlook.coom <arinaradochinatyuiu@mail.ru>
2025-10-26 01:17:51 +0300

    .

---
+++ Tyuiu.RadochinaAP.Sprint3.Task6.V8.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.RadochinaAP.Sprint3.Task6.V8.Lib
+{
+    public class DataService : ISprint3Task6V8
+    {
+        public int GetSumTheDivisors(int startValue, int stopValue)
+        {
+            int totalSum = 0;
+
+            for (int num = startValue; num <= stopValue; num++)
+            {
+                for (int divisor = 12; divisor <= num; divisor++) // Делители больше 11
+                {
+                    if (num % divisor == 0)
+                    {
+                        totalSum += divisor;
+                    }
+                }
+            }
+
+            return totalSum;
+        }
+    }
+}
---
+++ Tyuiu.RadochinaAP.Sprint3.Task6.V8.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.RadochinaAP.Sprint3.Task6.V8.Lib;
+namespace Tyuiu.RadochinaAP.Sprint3.Task6.V8.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumTheDivisors()
+        {
+            DataService ds = new DataService();
+            int startValue = 18;
+            int stopValue = 28;
+
+            int res = ds.GetSumTheDivisors(startValue, stopValue);
+
+            int wait = 292;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
