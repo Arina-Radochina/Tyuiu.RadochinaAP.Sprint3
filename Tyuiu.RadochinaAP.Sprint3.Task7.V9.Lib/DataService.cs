4f27687f3d982b26bcac0cb159419020fd149cee
Tyuiu.RadochinaAP@outlook.coom <arinaradochinatyuiu@mail.ru>
2025-10-28 21:48:37 +0300

    .

--- Tyuiu.RadochinaAP.Sprint3.Task7.V9.Lib/DataService.cs
+++ Tyuiu.RadochinaAP.Sprint3.Task7.V9.Lib/DataService.cs
@@ -12,7 +12,17 @@ namespace Tyuiu.RadochinaAP.Sprint3.Task7.V9.Lib
 
             for (int x = startValue; x <= stopValue; x++)
             {
-                valueArray[count] = Math.Round(Math.Sin(x), 2);
+                double denominator = Math.Cos(x) - 2 * x;
+
+                if (Math.Abs(denominator) < 0.000001)
+                {
+                    valueArray[count] = 0.00;
+                }
+                else
+                {
+                    double result = (2 * x - 3) / denominator + 5 * x - Math.Sin(x);
+                    valueArray[count] = Math.Round(result, 2);
+                }
                 count++;
             }
             return valueArray;
